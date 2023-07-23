1bc63b4f1fd266637e690c17cbbc83f30b01df1a
arpit gupta <mycontact.arpit@gmail.com>
2023-07-23 13:22:49 +0530

    structure of project

---
+++ premium-rental-car/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+const Main = () => {
+    return (
+        <div>
+            <Navbar />
+            <Footer />
+        </div>
+    )
+}
+
+export default Main
---
+++ premium-rental-car/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "../images/logo.png";
+import hamburger from "../images/hamburger_menu.png";
+
+export default function Navbar() {
+    return (
+        <div className="navbar-container">
+            <a href="#">
+                <img src={logo} alt="site logo" />
+            </a>
+            <div className="links">
+                <a href="#">HOME</a>
+                <a href="#">ABOUT</a>
+                <a href="#">OUR CARS</a>
+                <a href="#s">OPINIONS</a>
+                <a href="#">OUR TEAM</a>
+                <a href="#">CONTACT</a>
+            </div>
+            <button className="btn login">Log in</button>
+            <button className="btn register">Register</button>
+            {/* <div className="mobile">
+                <img src={hamburger} alt="mobile menu" />
+            </div> */}
+            <div className="btns"></div>
+        </div>
+    );
+}
